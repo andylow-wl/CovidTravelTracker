fd6d437d554b0900f78a60d1fa99e07a29264de3
Anna Tsolakou <anna.tsolakou@amadeus.com>
2021-06-29 17:57:05 +0200

    pass data to frontend and async marker

--- app.js
+++ app.js
@@ -15,14 +15,19 @@ var amadeus = new Amadeus({
   clientSecret: ''
 });
 
-
 app.get('/', (req,res,next) => {
-  const jsonCities = require("./cities");
-  res.render("index", {cities: jsonCities});
+  amadeus.referenceData.urls.checkinLinks.get({
+    airlineCode: 'BA'
+  }).then(function(response){
+    const jsonCities = require("./cities");
+    const jsonCountry = require("./country");
+    res.render('index', { cities: jsonCities, message: response.data, country: jsonCountry })
+  }).catch(function(error){
+    console.log(error.response);
+  });
 });
 
 app.get('/airportSearch/', function(req,res,next){
-
 amadeus.referenceData.locations.get({
   keyword: req.query.term,
   subType: 'AIRPORT,CITY'
@@ -34,5 +39,18 @@ amadeus.referenceData.locations.get({
   console.log(error.response);
 });
 });
+
+app.get('/search/', (req,res,next) => {
+  console.log("kala"+ req.query.term)
+  amadeus.referenceData.urls.checkinLinks.get({
+    airlineCode: 'IB'
+  }).then(function(response){
+    res.json(response.data);
+    console.log(response.data);
+  }).catch(function(error){
+    console.log("error");
+    console.log(error.response);
+  });
+  });
 
 app.listen(3000, () => console.log('App now running on port 3000!'));
