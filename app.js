ccffcee3580759db7ab2cc598081bc6ca5021cbd
Anna Tsolakou <anna.tsolakou@amadeus.com>
2021-06-29 11:13:39 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var Amadeus = require('amadeus');
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+var amadeus = new Amadeus({
+  clientId: '',
+  clientSecret: ''
+});
+
+
+app.get('/', (req,res,next) => {
+  const jsonCities = require("./cities");
+  res.render("index", {cities: jsonCities});
+});
+
+app.get('/airportSearch/', function(req,res,next){
+
+amadeus.referenceData.locations.get({
+  keyword: req.query.term,
+  subType: 'AIRPORT,CITY'
+}).then(function(response){
+  res.json(response.data);
+  console.log(response.data);
+}).catch(function(error){
+  console.log("error");
+  console.log(error.response);
+});
+});
+
+app.listen(3000, () => console.log('App now running on port 3000!'));
