dfad221831ee905de74ecb6a60e1e872b6d26851
Anna Tsolakou <anna.tsolakou@amadeus.com>
2021-06-30 17:30:24 +0200

    build async calls per city

--- app.js
+++ app.js
@@ -1,56 +1,40 @@
 var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
 var Amadeus = require('amadeus');
-
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
 
 var amadeus = new Amadeus({
   clientId: '',
   clientSecret: ''
 });
 
-app.get('/', (req,res,next) => {
-  amadeus.referenceData.urls.checkinLinks.get({
-    airlineCode: 'BA'
-  }).then(function(response){
+var app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get('/', (req, res, next) => {
+  // this API call will be replaced by the search by country
+  amadeus.media.files.generatedPhotos.get({
+    category: 'BEACH'
+  }).then(function (response) {
     const jsonCities = require("./cities");
+    // this file we won't need it as we will take the data from the API
     const jsonCountry = require("./country");
     res.render('index', { cities: jsonCities, message: response.data, country: jsonCountry })
-  }).catch(function(error){
+  }).catch(function (error) {
     console.log(error.response);
   });
 });
 
-app.get('/airportSearch/', function(req,res,next){
-amadeus.referenceData.locations.get({
-  keyword: req.query.term,
-  subType: 'AIRPORT,CITY'
-}).then(function(response){
-  res.json(response.data);
-  console.log(response.data);
-}).catch(function(error){
-  console.log("error");
-  console.log(error.response);
-});
-});
-
-app.get('/search/', (req,res,next) => {
-  console.log("kala"+ req.query.term)
-  amadeus.referenceData.urls.checkinLinks.get({
-    airlineCode: 'IB'
-  }).then(function(response){
+app.get('/search/', (req, res, next) => {
+  // the cityCode can be found at req.query.cityCode 
+  amadeus.media.files.generatedPhotos.get({
+    category: 'BEACH'
+  }).then(function (response) {
     res.json(response.data);
     console.log(response.data);
-  }).catch(function(error){
+  }).catch(function (error) {
     console.log("error");
     console.log(error.response);
   });
-  });
+});
 
 app.listen(3000, () => console.log('App now running on port 3000!'));
