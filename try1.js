d2aa5caf87410f3dbcaf7c9c2958a4f03a3f6886
andylow1704 <65076410+andylow1704@users.noreply.github.com>
2022-03-13 18:03:37 +0800

    Now able to print the json output

--- try1.js
+++ try1.js
@@ -1,20 +1,27 @@
-function getDetails(code){    
-    var Amadeus = require('amadeus');
+var Amadeus = require('amadeus');
 
-    var amadeus = new Amadeus({
+var amadeus = new Amadeus({
     clientId: 'uDGFRwqjN9TMyagU3pwL4Yv53iUZzXN3',
     clientSecret: 'FjthFeiXCnw4KX9D'
     });
 
-    // Travel Restrictions API by country
-    var output = amadeus.client.get('/v1/duty-of-care/diseases/covid19-area-report',
+
+  
+
+var code = "US";
+    
+// Travel Restrictions API by country
+var output = amadeus.client.get('/v1/duty-of-care/diseases/covid19-area-report',
     { countryCode: code })
     .then(function (response) {
         //const jsonCities = require("./cities");
         var json = response.data
-        return json;
+        console.log(json);
         //console.log(jsonCities);
-    }).catch(function (error) {
+      }).catch(function (error) {
         console.log(error.response);
-    });
-}
+      });
+    
+ 
+
+
