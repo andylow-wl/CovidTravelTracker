9eb531313742d7c0bcf9db2f8b2c04f734ffd6df
Melvin Dio Haryadi <dio.haryadiii@gmail.com>
2022-04-03 00:09:33 +0800

    Added firebase.js file and AddDataFS.vue

---
+++ src/components/AddDataFS.vue
@@ -0,0 +1,46 @@
+<template>
+    <button @click="fetchCovidCase()">test</button>
+    <div>{{CN.data}}</div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+    name : 'AddDataFS',
+    components : {},
+    data() {
+        return  {
+            AU : '',
+            CN : '',
+            ID : '',
+            JP : '',
+            KR : '',
+            MY : '',
+            US : '',
+        }
+    },
+    methods : {
+        async  fetchCovidCase() {
+            var enddateObj = new Date()
+            var startdateObj = new Date(new Date().setDate(enddateObj.getDate()-30));
+            var enddate = enddateObj.toISOString();
+            var startdate = startdateObj.toISOString();
+            enddate = enddate.substring(0, enddate.indexOf('T')+1);
+            startdate = startdate.substring(0, startdate.indexOf('T')+1);
+            enddate += "00:00:00Z"
+            startdate += "00:00:00Z"
+            console.log(startdate, enddate)
+            await axios.get('https://api.covid19api.com/total/country/' + 'AU' + '/status/confirmed?from=' + startdate + '&to=' + enddate).then(response => (this.AU = response))
+            await axios.get('https://api.covid19api.com/total/country/' + 'CN' + '/status/confirmed?from=' + startdate + '&to=' + enddate).then(response => (this.CN = response))
+            await axios.get('https://api.covid19api.com/total/country/' + 'ID' + '/status/confirmed?from=' + startdate + '&to=' + enddate).then(response => (this.ID = response))
+            await axios.get('https://api.covid19api.com/total/country/' + 'JP' + '/status/confirmed?from=' + startdate + '&to=' + enddate).then(response => (this.JP = response))
+            await axios.get('https://api.covid19api.com/total/country/' + 'KR' + '/status/confirmed?from=' + startdate + '&to=' + enddate).then(response => (this.KR = response))
+            await axios.get('https://api.covid19api.com/total/country/' + 'MY' + '/status/confirmed?from=' + startdate + '&to=' + enddate).then(response => (this.MY = response))
+            await axios.get('https://api.covid19api.com/total/country/' + 'US' + '/status/confirmed?from=' + startdate + '&to=' + enddate).then(response => (this.US = response))
+        }
+    },
+    mounted() {
+        console.log(this.AU)
+    },
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <NavBar/>
   <EnterDestination/>
+  <AddDataFS/>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
 import EnterDestination from '@/components/EnterDestination.vue'
+import AddDataFS from '@/components/AddDataFS.vue'
 
 export default {
     name: 'App',
     components: {
         NavBar,
         EnterDestination,
+        AddDataFS,
     },
 }
 </script>
