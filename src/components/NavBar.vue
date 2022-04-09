a37995128a5cc4a6c9d2525eda75428f57726697
cheezq <65379131+cheezq@users.noreply.github.com>
2022-04-09 23:05:32 +0800

    Updated NavBar.vue CSS

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,12 @@
 <template>
-    <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/Comparison">Comparison</router-link>
+    <div class="container">
+        <router-link to="/" class="logo"><b><i>Group L2-20</i></b></router-link>
+        <nav>
+            <ul>
+                <li><router-link to="/">Home</router-link></li>
+                <li><router-link to="/Comparison">Compare Destinations</router-link></li>
+            </ul>
+        </nav>
     </div>
 </template>
 
@@ -12,17 +17,73 @@ export default {
 </script>
 
 <style scoped>
-    #nav {
-        padding: 30px;
-        text-align: center;
+    .container {
+        width: 80%;
+        display: inline-block;
+        vertical-align: middle;
+        padding: 0.2em;
     }
 
-    #nav a{
+    .logo {
+        float: left;
+        font-size: 1.4em;
+        text-decoration: none;
+        padding-top: 0.2em;
+        border: 0.1em;
+        border-style: solid;
+        border-color: black;
+    }
+
+    nav {
+        float: right;
+    }
+
+    nav ul {
+        margin: 0;
+        padding: 0;
+        list-style: none;
+    }
+
+    
+    nav li {
         font-weight:bold;
         color: #2c3e50;
+        display: inline-block;
+        margin-left: 2em;
+        padding-top: 0.5em;
+        position: relative;
     }
 
-    #nav a.router-link-exact-active{
-        color: #42b983;
+    nav a {
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+
+    nav a.router-link-exact-active{
+        content: '';
+        display: block;
+        height: 0.2em;
+        width: 100%;
+        background-color: #444;
+    }
+
+    nav a::before {
+        content: '';
+        display: block;
+        height: 0.5em;
+        background-color: #444;
+        
+        position: absolute;
+        top: 0;
+        width: 0%;
+        transition: all ease-in-out 250ms;
+    }
+
+    nav a:hover {
+        color: #F28C28;
+    }
+
+    nav a:hover::before {
+        width:100%;
     }
 </style>
